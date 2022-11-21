69c7df02c1ed5d14fe75e67e1f0d945ca954330c
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2022-11-21 20:46:42 +0000

    pretty much finished wiith footer

--- keylogger/src/App.vue
+++ keylogger/src/App.vue
@@ -1,29 +1,52 @@
 <template>
+  <header>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  </header>
   <router-view />
+  <footer>
+    <MainFooter/>
+  </footer>
+
 </template>
 
+<script>
+  import MainFooter from './components/MainFooter.vue';
+  export default {
+  name: "App",
+  components: {
+    MainFooter,
+  },
+};
+</script>
+
 <style lang="scss">
+@import "_variables";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $VueGrey;
 }
-
+header{
+  width:100%;
+  padding:2rem;
+  display:flex;
+  justify-content: center;
+  font-size:2em;
+}
 nav {
-  padding: 30px;
+  padding: 3rem;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $VueGrey;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $VueGreen;
     }
   }
 }
--- keylogger/src/views/HomeView.vue
+++ keylogger/src/views/HomeView.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld/>
-    <MainFooter/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import MainFooter from "@/components/MainFooter.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
-    MainFooter,
   },
 };
 </script>
+
